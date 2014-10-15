94ce008a959fda651b364986c14d8e551fd9f9fb
bioshrek <shrekwang1@gmail.com>
2014-10-15 17:02:18 +0000

    correct build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-sudo docker run -v /home/chianyu/flannel:/home/chianyu/flannel chianyu/flannel cp -rv /flannel-binaries /home/chianyu/flannel/
+sudo docker run -v /home/chianyu/flannel:/home/chianyu/flannel bioshrek/flannel cp -rv /flannel-binaries /home/chianyu/flannel/
